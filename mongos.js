5c621d76329079c6a63806ec5ec82666fad12fd4
Shrirama Kamath <shriramakamath2002@gmail.com>
2024-08-15 19:13:08 +0530

    add nothing

--- mongos.js
+++ mongos.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () =>{
 
     await mongoose.connect('mongodb://127.0.0.1:27017/blog_api');
-    console.log('connected to MOngoDB server');
+    console.log('connected to MongoDB server');
 }
 
 module.exports = connectDB;
