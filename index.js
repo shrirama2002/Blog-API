92c77a8a5d35f4e84dade03fd5c8b0fc20c3afb9
Shrirama Kamath <shriramakamath2002@gmail.com>
2024-08-17 19:44:24 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
 const express = require('express');
 const connectDB = require('./mongos.js');
-const Category = require('./models/Category.js');
+
+// const Category = require('./models/Category.js');
 
 connectDB();
 
 const app = express();
 
+const categoryRoute = require('./routes/categories.js');
 // it is very very important and it
 // allows us to read the request body
 
 app.use(express.json())
-//home route
-app.post('/categories',async (req,res) => {
-    try {
-        const cat = await Category.create(req.body);
-        res.status(201).json({
-            category : cat
-        });
-    }
-    catch (error){
-        res.status(400).json({
-            success: false,
-            message : "Something went wrong",
-            error : error
-        });
-    }
-});
 
-app.get('/categories',async(req,res) => {
-    try{
-            const categories = await Category.find();
-            res.status(200).json({
-                count : categories.length,
-                success : true,
-                categories: categories
-            });
-    }
-    catch(error){
-        res.status(400).json({
-            success: false,
-            message : "Something went wrong",
-            error : error
-        });
-    }
-});
+
+app.use('/categories', categoryRoute)
+
+
+//home route
+// app.post('/categories',async (req,res) => {
+//     try {
+//         const cat = await Category.create(req.body);
+//         res.status(201).json({
+//             category : cat
+//         });
+//     }
+//     catch (error){
+//         res.status(400).json({
+//             success: false,
+//             message : "Something went wrong",
+//             error : error
+//         });
+//     }
+// });
+
+// app.get('/categories',async(req,res) => {
+//     try{
+//             const categories = await Category.find();
+//             res.status(200).json({
+//                 count : categories.length,
+//                 success : true,
+//                 categories: categories
+//             });
+//     }
+//     catch(error){
+//         res.status(400).json({
+//             success: false,
+//             message : "Something went wrong",
+//             error : error
+//         });
+//     }
+// });
 
 
 
