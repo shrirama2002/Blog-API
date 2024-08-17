8bf2216a5099baa4d1092196914e96f01737684b
Shrirama Kamath <shriramakamath2002@gmail.com>
2024-08-17 23:20:42 +0530

    add a blog model

---
+++ models/Blog.js
@@ -0,0 +1,42 @@
+const { type } = require('express/lib/response')
+const mongoose = require('mongoose')
+
+const blogSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength:[5,"Title can not be less than 5 characters"],
+        maxLength:[25,"title can not exceed 25 characters "]
+    },
+    body:{
+        type: String,
+        required: [true,"Can not be empty"],
+        minLength:[250,"body can not be less than 250 characters"]
+    },
+    likes:{
+        type: Number,
+        default: 0,
+        required : [true, "Likes can not be empty"]
+
+    },
+    status: {
+        type: String,
+        enum: ["public","private"],
+        default: "private"
+    },
+    hashtag:{
+        type: String
+    }
+    // author: {
+    //     type: String,
+    //     minLength:[5,"Author name can not be less than 5 characters"],
+    //     maxLength: [25,"Author name can not exceed 25 characters "]
+    // }
+},{
+    timestamps : true
+})
+
+const blog = mongoose.model('Blog',blogSchema);
+
+module.exports = blog;
